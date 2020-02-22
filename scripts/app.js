22acb76972c750f2dd162a5c8ef4371cd9cf2214
carlos <cdeobaldia@gmail.com>
2020-02-22 12:35:02 +0100

    Add click on init

--- scripts/app.js
+++ scripts/app.js
@@ -55,10 +55,11 @@ function init() {
   // grab the record controls
   var rec = document.querySelector('.rec');
 
-
+  var visualSelect = document.getElementById("visual");
   //set up the different audio nodes we will use for the app
 
   var analyser1 = audioCtx.createAnalyser();
+  analyser1.fftSize = 256;
   analyser1.minDecibels = -90;
   analyser1.maxDecibels = -10;
   analyser1.smoothingTimeConstant = 0.85;
@@ -75,22 +76,7 @@ function init() {
 
   var preGain = audioCtx.createGain();
 
-  // distortion curve for the waveshaper, thanks to Kevin Ennis
-  // http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
 
-  function makeDistortionCurve(amount) {
-    var k = typeof amount === 'number' ? amount : 50,
-      n_samples = 44100,
-      curve = new Float32Array(n_samples),
-      deg = Math.PI / 180,
-      i = 0,
-      x;
-    for ( ; i < n_samples; ++i ) {
-      x = i * 2 / n_samples - 1;
-      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
-    }
-    return curve;
-  };
 
   // grab audio track via XHR for convolver node
 
@@ -107,9 +93,9 @@ function init() {
     var audioData = ajaxRequest.response;
 
     audioCtx.decodeAudioData(audioData, function(buffer) {
-        soundSource = audioCtx.createBufferSource();
-        convolver.buffer = buffer;
-      }, function(e){ console.log("Error with decoding audio data" + e.err);});
+      soundSource = audioCtx.createBufferSource();
+      convolver.buffer = buffer;
+    }, function(e){ console.log("Error with decoding audio data" + e.err);});
 
     //soundSource.connect(audioCtx.destination);
     //soundSource.loop = true;
@@ -125,160 +111,122 @@ function init() {
 
   var intendedWidth = document.querySelector('.wrapper').clientWidth;
 
+  var canvasFillColor = "red";
+
   canvas.setAttribute('width',intendedWidth);
 
-  var visualSelect = document.getElementById("visual");
 
-  var drawVisual;
+
+  var drawVisual;  //requestAnimationFrame(f)
 
   //main block for doing the audio recording
 
   if (navigator.mediaDevices.getUserMedia) {
-     console.log('getUserMedia supported.');
-     var constraints = {audio: true}
-     navigator.mediaDevices.getUserMedia (constraints)
-        .then(
-          function(stream) {
-             source = audioCtx.createMediaStreamSource(stream);
-             //source2 = audioCtx.createMediaStreamSource(stream);
-             source.connect(preGain);
-             //source2.connect(analyser1);
-             preGain.connect(distortion);
-             //analyser1.connect(distortion);
-             distortion.connect(biquadFilter);
-             biquadFilter.connect(gainNode);
-             convolver.connect(gainNode);
-             gainNode.connect(analyser2);
-             analyser2.connect(audioCtx.destination);
+    console.log('getUserMedia supported.');
+    var constraints = {audio: true}
+    navigator.mediaDevices.getUserMedia (constraints)
+    .then(
+      function(stream) {
+        source = audioCtx.createMediaStreamSource(stream);
+        source.connect(preGain);
+        preGain.connect(analyser1);
+        analyser1.connect(distortion);
+        distortion.connect(biquadFilter);
+        biquadFilter.connect(gainNode);
+        convolver.connect(gainNode);
+        gainNode.connect(analyser2);
+        analyser2.connect(audioCtx.destination);
 
-             showPeakF();
-          	 visualize();
-             voiceChange();
-        })
-        .catch( function(err) { console.log('The following gUM error occured: ' + err);})
-  } else {
-     console.log('getUserMedia not supported on your browser!');
-  }
-
-  function showPeakF(){
-   analyser1.fftSize = 256;
-   var bufferLengthAlt2 = analyser1.frequencyBinCount;
-   console.log(bufferLengthAlt2);
-   var dataArrayAlt2 = new Uint8Array(bufferLengthAlt2);
-   analyser1.getByteFrequencyData(dataArrayAlt2);
-   var pkT=Math.max.apply(null,dataArrayAlt2);
-   function indexOfMax(arr) {
-     if (arr.length === 0) {
-         return -1;
-     }
-     var max = arr[0];
-     var maxIndex = 0;
-     for (var i = 1; i < arr.length; i++) {
-         if (arr[i] > max) {
-             maxIndex = i;
-             max = arr[i];
-         }
-     }
-     return maxIndex;
+        audioMeter1(); //From visualize()
+        voiceChange();
+        visualize();
+      })
+      .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+    } else {
+      console.log('getUserMedia not supported on your browser!');
     }
-    var pkTi = indexOfMax(dataArrayAlt2);
-    var binWidth = (22050 / bufferLengthAlt2);
-    peakText2.value = (pkTi*binWidth).toFixed(2);
-  //
-  console.log("!");
-  }
 
-  function visualize() {
-    WIDTH = canvas.width;
-    HEIGHT = canvas.height;
+    function audioMeter1(){
 
+      var bufferLengthAlt2 = analyser1.frequencyBinCount;
+      console.log('analyser1 bins: ' + bufferLengthAlt2);
+      var dataArrayAlt2 = new Uint8Array(bufferLengthAlt2);
 
-    var visualSetting = visualSelect.value;
-    console.log(visualSetting);
+      var drawMeter1 = function(){
+        getData = requestAnimationFrame(audioMeter1);
+        analyser1.getByteFrequencyData(dataArrayAlt2);
 
-    if(visualSetting === "sinewave") {
-      analyser2.fftSize = 2048;
-      var bufferLength = analyser2.fftSize;
-      console.log(bufferLength);
-      var dataArray = new Uint8Array(bufferLength);
+        //Get data
+        //var pkT=Math.max.apply(null,dataArrayAlt);
+        //var pkTi = indexOfMax(dataArrayAlt);
+        var maxIndex2 = getMaxIArray(dataArrayAlt2); //[m,i]
 
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-      var draw = function() {
-
-        drawVisual = requestAnimationFrame(draw);
-
-        analyser2.getByteTimeDomainData(dataArray);
-
-        canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-        canvasCtx.lineWidth = 2;
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-        canvasCtx.beginPath();
-
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
-
-        for(var i = 0; i < bufferLength; i++) {
-
-          var v = dataArray[i] / 128.0;
-          var y = v * HEIGHT/2;
-
-          if(i === 0) {
-            canvasCtx.moveTo(x, y);
-          } else {
-            canvasCtx.lineTo(x, y);
-          }
-
-          x += sliceWidth;
-        }
-
-        canvasCtx.lineTo(canvas.width, canvas.height/2);
-        canvasCtx.stroke();
+        var binWidth = (22050 / bufferLengthAlt2);
+        peakText2.value = (maxIndex2[1] * binWidth).toFixed(2);
       };
 
-      draw();
+      drawMeter1();
+    }
 
-    } else if(visualSetting == "frequencybars") {
-      analyser2.fftSize = 4096;
-      var bufferLengthAlt = analyser2.frequencyBinCount;
-      console.log(bufferLengthAlt);
-      var dataArrayAlt = new Uint8Array(bufferLengthAlt);
+    function visualize() {
+      WIDTH = canvas.width;
+      HEIGHT = canvas.height;
 
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+      var draw = function() {
+        drawVisual = requestAnimationFrame(draw);
+          canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-      var drawAlt = function() {
-        drawVisual = requestAnimationFrame(drawAlt);
+        if(visualSelect.value === "sinewave") {
+          analyser1.fftSize = 256;
+          var bufferLength = analyser2.fftSize;
+          console.log(bufferLength);
+          var dataArray = new Uint8Array(bufferLength);
+
+          analyser2.getByteTimeDomainData(dataArray);
+
+          canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+          canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+          canvasCtx.lineWidth = 2;
+          canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+          canvasCtx.beginPath();
+
+          var sliceWidth = WIDTH * 1.0 / bufferLength;
+          var x = 0;
+
+          for(var i = 0; i < bufferLength; i++) {
+
+            var v = dataArray[i] / 128.0;
+            var y = v * HEIGHT/2;
+
+            if(i === 0) {
+              canvasCtx.moveTo(x, y);
+            } else {
+              canvasCtx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+          }
+
+          canvasCtx.lineTo(canvas.width, canvas.height/2);
+          canvasCtx.stroke();
+        }
+        else if(visualSelect.value === "frequencybars") {
+        analyser1.fftSize = 2048;
+        var bufferLengthAlt = analyser2.frequencyBinCount;
+        console.log(bufferLengthAlt);
+        var dataArrayAlt = new Uint8Array(bufferLengthAlt);
+
+        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+        // SizeOf dataArray should === bufferLength, always
+
 
         analyser2.getByteFrequencyData(dataArrayAlt);
 
-  //
-          var pkT=Math.max.apply(null,dataArrayAlt);
-          function indexOfMax(arr) {
-            if (arr.length === 0) {
-                return -1;
-            }
-
-            var max = arr[0];
-            var maxIndex = 0;
-
-            for (var i = 1; i < arr.length; i++) {
-                if (arr[i] > max) {
-                    maxIndex = i;
-                    max = arr[i];
-                }
-            }
-
-            return maxIndex;
-          }
-          var pkTi = indexOfMax(dataArrayAlt);
-
-          var binWidth = (22050 / bufferLengthAlt);
-          peakText.value = (pkTi*binWidth).toFixed(2);
-
-        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        //Draw spectrum
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)'; //Background
         canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
         var barWidth = (WIDTH / bufferLengthAlt) * 2.5;
@@ -292,84 +240,128 @@ function init() {
           canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
 
           x += barWidth + 1;
+
+          //Get data
+          //var pkT=Math.max.apply(null,dataArrayAlt);
+          //var pkTi = indexOfMax(dataArrayAlt);
+          var maxIndex = getMaxIArray(dataArrayAlt); //[m,i]
+
+          var binWidth = (22050 / bufferLengthAlt);
+          peakText.value = (maxIndex[1] * binWidth).toFixed(2);
+
         }
-      };
+      }
+      else if(visualSelect.value === "off") {
+        canvasCtx.fillStyle = canvasFillColor;
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      }};
 
-      drawAlt();
-
-    } else if(visualSetting == "off") {
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-      canvasCtx.fillStyle = "red";
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      draw();
     }
 
-  }
+    function voiceChange() {
 
-  function voiceChange() {
+      distortion.oversample = '4x';
+      biquadFilter.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
 
-    distortion.oversample = '4x';
-    biquadFilter.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
+      var voiceSetting = voiceSelect.value;
+      console.log(voiceSetting);
 
-    var voiceSetting = voiceSelect.value;
-    console.log(voiceSetting);
+      //when convolver is selected it is connected back into the audio path
+      if(voiceSelect.value === "convolver") {
+        biquadFilter.disconnect(0);
+        biquadFilter.connect(convolver);
+      } else {
+        biquadFilter.disconnect(0);
+        biquadFilter.connect(gainNode);
 
-    //when convolver is selected it is connected back into the audio path
-    if(voiceSetting == "convolver") {
-      biquadFilter.disconnect(0);
-      biquadFilter.connect(convolver);
-    } else {
-      biquadFilter.disconnect(0);
-      biquadFilter.connect(gainNode);
-
-      if(voiceSetting == "distortion") {
-        distortion.curve = makeDistortionCurve(400);
-      } else if(voiceSetting == "biquad") {
-        biquadFilter.type = "lowshelf";
-        biquadFilter.frequency.setTargetAtTime(1000, audioCtx.currentTime, 0)
-        biquadFilter.gain.setTargetAtTime(25, audioCtx.currentTime, 0)
-      } else if(voiceSetting == "off") {
-        console.log("Voice settings turned off");
+        if(voiceSelect.value === "distortion") {
+          distortion.curve = makeDistortionCurve(400);
+        } else if(voiceSetting === "biquad") {
+          biquadFilter.type = "lowshelf";
+          biquadFilter.frequency.setTargetAtTime(1000, audioCtx.currentTime, 0)
+          biquadFilter.gain.setTargetAtTime(25, audioCtx.currentTime, 0)
+        } else if(voiceSelect.value === "off") {
+          //console.log("Voice settings turned off");
+        }
       }
     }
+
+    // event listeners to change visualize and voice settings
+
+    visualSelect.onchange = function() {
+      //window.cancelAnimationFrame(drawVisual); //Because the animation frame is buffered
+      //visualize();
+      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+      console.log('visual: ' + visualSelect.value);
+    };
+
+    voiceSelect.onchange = function() {
+      //voiceChange();
+      console.log('voice: ' + voiceSelect.value);
+    };
+
+    mute.onclick = voiceMute;
+
+    function voiceMute() {
+      if(mute.id === "") {
+        gainNode.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
+        mute.id = "activated";
+        mute.innerHTML = "Unmute Output";
+      } else {
+        gainNode.gain.setTargetAtTime(1, audioCtx.currentTime, 0)
+        mute.id = "";
+        mute.innerHTML = "Mute Output";
+      }
+    }
+
+    // Record and stop controls
+    rec.onclick = voiceRecord;
+    function voiceRecord(){
+      if(rec.id === "") {
+        preGain.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
+        rec.id = "activated";
+        rec.innerHTML = "Record";
+      } else {
+        preGain.gain.setTargetAtTime(1, audioCtx.currentTime, 0)
+        rec.id = "";
+        rec.innerHTML = "Stop Recording";
+      }
+    }
+
   }
 
-  // event listeners to change visualize and voice settings
-
-  visualSelect.onchange = function() {
-    window.cancelAnimationFrame(drawVisual);
-    visualize();
+  // distortion curve for the waveshaper, thanks to Kevin Ennis
+  // http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
+  function makeDistortionCurve(amount) {
+    var k = typeof amount === 'number' ? amount : 50,
+    n_samples = 44100,
+    curve = new Float32Array(n_samples),
+    deg = Math.PI / 180,
+    i = 0,
+    x;
+    for ( ; i < n_samples; ++i ) {
+      x = i * 2 / n_samples - 1;
+      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+    }
+    return curve;
   };
 
-  voiceSelect.onchange = function() {
-    voiceChange();
-  };
-
-  mute.onclick = voiceMute;
-
-  function voiceMute() {
-    if(mute.id === "") {
-      gainNode.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
-      mute.id = "activated";
-      mute.innerHTML = "Unmute Output";
-    } else {
-      gainNode.gain.setTargetAtTime(1, audioCtx.currentTime, 0)
-      mute.id = "";
-      mute.innerHTML = "Mute Output";
+  function getMaxIArray(_dataArrayBuffer) {
+    var maxIndex = new Array(2).fill(0); //Persistent ()?!)
+    if (_dataArrayBuffer.length === 0) {
+      return maxIndex;
     }
-  }
 
-  // Record and stop controls
-  rec.onclick = voiceRecord;
-  function voiceRecord(){
-    if(rec.id === "") {
-      preGain.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
-      rec.id = "activated";
-      rec.innerHTML = "Record";
-    } else {
-      preGain.gain.setTargetAtTime(1, audioCtx.currentTime, 0)
-      rec.id = "";
-      rec.innerHTML = "Stop Recording";
+    var max = _dataArrayBuffer[0];
+    var mIndex = _dataArrayBuffer[1];
+
+    for (var i = 1; i < _dataArrayBuffer.length; i++) {
+      if (_dataArrayBuffer[i] > max) {
+        maxIndex[0] = _dataArrayBuffer[i];
+        maxIndex[1] = i;
+      }
     }
-  }
 
-}
+    return maxIndex;
+  }
